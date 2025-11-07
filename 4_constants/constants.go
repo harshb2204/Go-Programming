e7167d6bebf9e5d05b3ad3a5decf7114bc201182
Harsh <harshb2204@gmail.com>
2025-11-07 14:15:33 +0530

    Document Go constants

---
+++ 4_constants/constants.go
@@ -0,0 +1,18 @@
+package main
+
+const age = 21
+
+// name:= "harsh" ==> not allowed
+
+
+func main(){
+	// const name = "golang"
+	// const age = 30
+
+	const (
+
+		port = 5000
+		host = "localhost"
+	)
+
+}
