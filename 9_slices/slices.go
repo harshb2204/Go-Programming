e2c4ee1f5688e24b4d0b8a25d2eba61955369b13
Harsh <harshb2204@gmail.com>
2025-11-10 19:51:08 +0530

    slices

---
+++ 9_slices/slices.go
@@ -0,0 +1,28 @@
+package main
+
+// dynamic, most used construct, useful methods
+
+func main() {
+
+	// uninitialized slice is nil and len is 0
+	// var nums []int
+
+	// var nums = make([]int, 0, 5) // length 2 capacity 5 this makes array have 2,  0 elements ans as we append
+	// it appends after the zeroes thus length is kept 0
+	// capacity -> max numbers of elements can fit
+	// fmt.Println(nums)
+	// nums = append(nums, 1)
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+
+	// arr:= []int{}
+	// fmt.Println(cap(arr))
+
+	// var arr1 = make([]int, 0, 5)
+	// arr1 = append(arr1, 1)
+
+	// var arr2 = make([]int, len(arr1))
+
+	// copy(arr1, arr2)
+
+}
